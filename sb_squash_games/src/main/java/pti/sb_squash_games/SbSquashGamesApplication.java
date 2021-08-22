59d458c7cbe50386cb10768cc368729d14cc5c3e
Tamas Beszedics <beszedicshun@gmail.com>
2021-08-22 22:15:45 +0200

    init

---
+++ sb_squash_games/src/main/java/pti/sb_squash_games/SbSquashGamesApplication.java
@@ -0,0 +1,13 @@
+package pti.sb_squash_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSquashGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSquashGamesApplication.class, args);
+	}
+
+}
---
+++ sb_squash_games/src/test/java/pti/sb_squash_games/SbSquashGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package pti.sb_squash_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSquashGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
